baa686326cd8a7c5193a149c8fac4eb5c1ebf75c
Lailatef <54871132+Lailatef@users.noreply.github.com>
2021-09-07 22:00:28 -0400

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,13 @@
+### welcome_assignment_answers
+### Input - All eight questions given in the assignment.
+### Output - The right answer for the specific question.
+
+def welcome_assignment_answers(question):
+    # The student doesn't have to follow the skeleton for this assignment.
+    # Another way to implement it is using "case" statements similar to C.
+    if question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+    return (answer)
+
